f9554fc278ae43347a0db41b206c95998d7e7b87
zholdoshov <nur.joldoshov@gmail.com>
2024-05-29 19:51:59 -0700

    4akjshd

--- test/com/example/FizzBuzzProcessorTest.java
+++ test/com/example/FizzBuzzProcessorTest.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
