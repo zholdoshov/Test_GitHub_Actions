14c5aaa5942507af931838fb5702279ea9bc7637
zholdoshov <nur.joldoshov@gmail.com>
2024-05-29 19:27:39 -0700

    fc

---
+++ README-Project-Setup.txt
@@ -0,0 +1,14 @@
+Unzip file into a directory.
+Open the TW2-com.example.FizzBuzzProcessor folder as an IntelliJ project
+File | Project Structure | Project | SDK  (Choose "coretto-21" in dropdown list, or EDIT button, "+" to add New SDK via download) "Amazon coretto 21.0.2"
+Set the SDK to coretto-21 - that works, and is the one I tested it with. 
+Set Language Level to 20. 
+
+Project folders and Maven Dependencies:
+   pom.xml sets [Sources Root] to "src" and [Test Sources Root] to "test" folders.
+   pom.xml specifies version of JUnit to be 5.10.2
+   No changes should be required.
+
+
+
+
---
+++ src/com/example/FizzBuzzProcessor.java
@@ -0,0 +1,28 @@
+package com.example;// import jdk.nashorn.internal.runtime.regexp.joni.exception.ValueException;
+
+public class FizzBuzzProcessor
+{
+    public static void main()
+    {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(convert(i));
+        }
+    }
+
+    public static String convert(int fizzBuzz) throws IllegalArgumentException {
+
+        if (fizzBuzz <= 0) {
+            throw new IllegalArgumentException();
+        }
+        if (fizzBuzz % 15 == 0) {
+            return "FizzBuzz";
+        }
+        if (fizzBuzz % 3 == 0) {
+            return "Fizz";
+        }
+        if (fizzBuzz % 5 == 0) {
+            return "Buzz";
+        }
+        return String.valueOf(fizzBuzz);
+    }
+}
